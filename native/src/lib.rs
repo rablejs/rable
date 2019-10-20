7fcdd27b584dc6ce35cfc0229760eb1592cd0573
Charles Pierce <cpierce.grad@gmail.com>
2019-10-20 12:13:20 -0700

    Add infrastructure files

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,12 +1,7 @@
-#[macro_use]
-extern crate neon;
-
 use neon::prelude::*;
 
 fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
     Ok(cx.string("hello node"))
 }
 
-register_module!(mut cx, {
-    cx.export_function("hello", hello)
-});
+register_module!(mut cx, { cx.export_function("hello", hello) });
