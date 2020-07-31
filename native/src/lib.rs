a76830ee0255fae629cb0408d9fe72e2b0f738b0
Charles Pierce <cpierce.grad@gmail.com>
2020-07-30 22:18:08 -0700

    Experimenting with 'require' from native code

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -4,4 +4,21 @@ fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
     Ok(cx.string("hello node"))
 }
 
-register_module!(mut cx, { cx.export_function("hello", hello) });
+fn semver_valid(mut cx: FunctionContext) -> JsResult<JsValue> {
+    let require: Handle<JsFunction> = cx.argument(0)?;
+    let null = cx.null();
+    let args = vec![cx.string("semver")];
+    let semver: Handle<JsObject> = require
+        .call(&mut cx, null, args)?
+        .downcast_or_throw(&mut cx)?;
+    let valid: Handle<JsFunction> = semver.get(&mut cx, "valid")?.downcast_or_throw(&mut cx)?;
+
+    let version = cx.argument_opt(1);
+    valid.call(&mut cx, semver, version)
+}
+
+register_module!(mut cx, {
+    cx.export_function("hello", hello)?;
+    cx.export_function("semver_valid", semver_valid)?;
+    Ok(())
+});
